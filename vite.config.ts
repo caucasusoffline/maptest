453a1cdb6e0574262f5bf529ffe1cb1e15ac4fee
caucasusoffline <irlmch@gmail.com>
2026-07-09 13:38:09 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(({ mode }) => {
   return {
-    base: mode === 'production' ? '/georgia-speedtest-map/' : '/',
+    base: mode === 'production' ? '/maptest/' : '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
